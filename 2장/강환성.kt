74a9a159e8bd6473a13f469479b5b5eef94b8811
khs <parts1123@gmail.com>
2023-09-24 19:52:26 +0900

    강환성/ 2장> 람다표현법 추가

--- 2장/강환성.kt
+++ 2장/강환성.kt
@@ -2,13 +2,11 @@ package codespitz_study_kotlinfunctionalprogramming.`2장`
 
 // 연습문제 2.1
 fun fib(i: Int): Int {
-    tailrec fun go(n: Int, acc1: Int, acc2: Int): Int {
-        return if(n == 0) acc2
-        else {
-            go(n-1, acc2, acc1 + acc2)
-        }
+    tailrec fun go(n: Int, a: Int= 0, b: Int= 1): Int {
+        return if (n == 0) a else go(n - 1, b, a + b)
     }
-    return go(i, 0, 1)
+
+    return go(i)
 }
 
 // 연습문제 2.2
@@ -42,23 +40,17 @@ fun <A, B, C> compose(f: (B)-> C, g: (A)-> B): (A)-> C= {
 
 fun main() {
     println("2. 코틀린으로 함수형 프로그래밍 시작하기")
-    println("2.1: "+ listOf(0, 1, 2, 3, 4, 5, 6, 7).map { fib(it) })
+    println("2.1: "+ listOf(0, 1, 2, 3, 4, 5, 6, 7, 8).map { fib(it) })
 
     println("2.2: "+ isSorted(listOf(1, 2, 3)) { a, b -> a <= b })
     println("2.2: "+ isSorted(listOf("1", "3", "2")) { a, b -> a <= b })
 
     println("2.3: "+ curry(fun (a: Int, b: Int)= a+b)(1)(2))
-    println("2.3: "+ curry { a: Int, b: Int ->
-        a + b
-    }(1)(2))
+    println("2.3: "+ curry { a: Int, b: Int -> a + b }(1)(2))
 
     println("2.4: "+ uncurry(fun (a: Int)= fun (b: Int)= a+b)(1, 2))
-    println("2.4: "+ uncurry { a: Int-> {
-        b: Int-> a+b
-    } }(1, 2))
+    println("2.4: "+ uncurry { a: Int-> { b: Int-> a+b } }(1, 2))
 
     println("2.5: "+ compose(fun (b: Int)= b+1, fun (a: Int)= a+1)(1))
-    println("2.5: "+ compose({ b: Int-> b+1 }) { a: Int->
-        a + 1
-    }(1))
+    println("2.5: "+ compose({ b: Int-> b+1 }) { a: Int-> a + 1 }(1))
 }
