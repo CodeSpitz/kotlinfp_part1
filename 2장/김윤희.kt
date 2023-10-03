571ec62d6ca4c63a99077b7fcc1331f90e006b3f
kyh1126 <kyh901126@nate.com>
2023-10-04 01:20:50 +0900

    feat: 2.5 함수 합성 함수 추가

--- 2장/김윤희.kt
+++ 2장/김윤희.kt
@@ -25,3 +25,5 @@ fun <A, B, C> uncurry(f: (A) -> (B) -> C): (A, B) -> C =
     { a: A, b: B -> f(a)(b) }
 
 // 2.5
+fun <A, B, C> compose(f: (B) -> C, g: (A) -> B): (A) -> C =
+    { a: A -> f(g(a)) }
