8a5c87eeb484facf8c909101e1555b708a480e37
kyh1126 <kyh901126@nate.com>
2023-10-09 22:25:44 +0900

    feat: 3.2 setHead 함수 추가

--- 3장/김윤희.kt
+++ 3장/김윤희.kt
@@ -4,3 +4,10 @@ fun <A> tail(xs: List<A>): List<A> =
         is Nil -> throw IllegalStateException() // Nil
         is Cons -> xs.tail
     }
+
+// 3.2
+fun <A> setHead(xs: List<A>, x: A): List<A> =
+    when (xs) {
+        is Nil -> throw IllegalStateException() // Nil
+        is Cons -> Cons(x, xs.tail)
+    }
