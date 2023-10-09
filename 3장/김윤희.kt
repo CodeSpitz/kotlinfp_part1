0589ab3a829a6fa37b9de2ce8fab1737ab0bdd80
kyh1126 <kyh901126@nate.com>
2023-10-09 22:32:55 +0900

    feat: 3.3 drop 함수 추가

--- 3장/김윤희.kt
+++ 3장/김윤희.kt
@@ -11,3 +11,13 @@ fun <A> setHead(xs: List<A>, x: A): List<A> =
         is Nil -> throw IllegalStateException() // Nil
         is Cons -> Cons(x, xs.tail)
     }
+
+// 3.3
+fun <A> drop(l: List<A>, n: Int): List<A> {
+    if (n == 0) return l
+
+    return when (l) {
+        is Nil -> throw IllegalStateException() // Nil
+        is Cons -> drop(l.tail, n - 1)
+    }
+}
