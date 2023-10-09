f497a44de80c07462d8571645a18e1147bbc1ddf
kyh1126 <kyh901126@nate.com>
2023-10-09 23:00:38 +0900

    feat: 3.5 init 함수 추가

--- 3장/김윤희.kt
+++ 3장/김윤희.kt
@@ -29,3 +29,11 @@ fun <A> dropWhile(l: List<A>, f: (A) -> Boolean): List<A> {
         is Cons -> if (f(l.head)) dropWhile(l.tail, f) else l
     }
 }
+
+// 3.5
+fun <A> init(l: List<A>): List<A> {
+    return when (l) {
+        is Nil -> throw IllegalStateException()
+        is Cons -> if (l.tail == Nil) return Nil else Cons(l.head, init(l.tail))
+    }
+}
