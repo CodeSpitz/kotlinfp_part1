4aa88c31f97153a580fee439b4c46e38df49888d
kyh1126 <kyh901126@nate.com>
2023-10-11 02:27:13 +0900

    feat: 3.24 size 함수 추가

--- 3장/김윤희.kt
+++ 3장/김윤희.kt
@@ -182,3 +182,10 @@ tailrec fun <A> hasSubsequence(xs: List<A>, sub: List<A>): Boolean =
             else hasSubsequence(xs.tail, sub)
         }
     }
+
+// 3.24
+fun <A> size(tree: Tree<A>): Int =
+    when (tree) {
+        is Leaf -> 1
+        is Branch -> 1 + size(tree.left) + size(tree.right)
+    }
