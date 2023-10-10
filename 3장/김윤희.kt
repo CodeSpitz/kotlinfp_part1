f0fba1f76f3213953a5f103d17acbbc715f13a1f
kyh1126 <kyh901126@nate.com>
2023-10-11 02:47:38 +0900

    feat: 3.28 fold 함수 추가

--- 3장/김윤희.kt
+++ 3장/김윤희.kt
@@ -210,3 +210,22 @@ fun <A, B> map(tree: Tree<A>, f: (A) -> B): Tree<B> =
         is Leaf -> Leaf(f(tree.value))
         is Branch -> Branch(map(tree.left, f), map(tree.right, f))
     }
+
+// 3.28
+fun <A, B> fold(ta: Tree<A>, l: (A) -> B, b: (B, B) -> B): B =
+    when (ta) {
+        is Leaf -> l(ta.value)
+        is Branch -> b(fold(ta.left, l, b), fold(ta.right, l, b))
+    }
+
+fun <A> sizeF(ta: Tree<A>): Int =
+    fold(ta, { 1 }, { b1, b2 -> 1 + b1 + b2 })
+
+fun maximumF(ta: Tree<Int>): Int =
+    fold(ta, { a -> a }, { b1, b2 -> maxOf(b1, b2) })
+
+fun <A> depthF(ta: Tree<A>): Int =
+    fold(ta, { 0 }, { b1, b2 -> maxOf(1 + b1, 1 + b2) })
+
+fun <A, B> mapF(ta: Tree<A>, f: (A) -> B): Tree<B> =
+    fold(ta, { a -> Leaf(f(a)) }, { b1: Tree<B>, b2: Tree<B> -> Branch(b1, b2) })
