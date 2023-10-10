24e16b2d919c90107ac5c49a7b7596b0825f4aa5
kyh1126 <kyh901126@nate.com>
2023-10-11 01:31:03 +0900

    feat: 3.22 zipWith 함수 추가

--- 3장/김윤희.kt
+++ 3장/김윤희.kt
@@ -152,3 +152,12 @@ fun add(xa: List<Int>, xb: List<Int>): List<Int> =
         }
     }
 
+// 3.22
+fun <A> zipWith(xa: List<A>, xb: List<A>, f: (A, A) -> A): List<A> =
+    when (xa) {
+        is Nil -> Nil
+        is Cons -> when (xb) {
+            is Nil -> Nil
+            is Cons -> Cons(f(xa.head, xb.head), zipWith(xa.tail, xb.tail, f))
+        }
+    }
