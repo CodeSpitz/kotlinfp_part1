58f6f52c04183a1ede8473090cf5fcee9469ef4f
zoops <jhbaik@gmail.com>
2023-10-18 21:32:22 +0900

    백지훈 4장 연습문제

--- 4장/백지훈.kt
+++ 4장/백지훈.kt
@@ -1,3 +1,123 @@
+sealed class Option<out A> {
+
+    companion object {
+        fun <A> of(a: A): Option<A> = Some(a)
+        fun <A> empty(): Option<A> = None
+    }
+}
+
+data class Some<out A>(val get: A) : Option<A>()
+object None : Option<Nothing>()
+
+// 4-1 option 에 대한 모든 함수 구현
+fun <A, B> Option<A>.map(f: (A) -> B): Option<B> =
+    when (this) {
+        is None -> None
+        is Some -> Some(f(this.get))
+    }
+
+fun <A, B> Option<A>.flatMap(f: (A) -> Option<B>): Option<B> =
+    when (this) {
+        is None -> None
+        is Some -> f(this.get)
+    }
+
+fun <A> Option<A>.getOrElse(default: () -> A): A =
+    when (this) {
+        is None -> default()
+        is Some -> this.get
+    }
+
+fun <A> Option<A>.orElse(ob: () -> Option<A>): Option<A> =
+    when (this) {
+        is None -> ob()
+        is Some -> this
+    }
+
+fun <A> Option<A>.filter(f: (A) -> Boolean): Option<A> =
+    when (this) {
+        is None -> None
+        is Some -> if (f(this.get)) this else None
+    }
+
+
+// 4-2 variance 함수 구현 (flatMap 사용)
+fun variance(xs: List<Double>): Option<Double> =
+    mean(xs).flatMap {
+            m -> mean(
+        map(xs) { x -> Math.pow(x - m, 2.0) }
+    )
+    }
+
+fun mean(xs: List<Double>): Option<Double> =
+    if (isEmpty(xs)) None
+    else Some(sum(xs) / size(xs))
+
+// 4-3 map2 함수 구현
+fun <A, B, C> map2(a: Option<A>, b: Option<B>, f: (A, B) -> C): Option<C> =
+    a.flatMap { a1 -> b.map { b1 -> f(a1, b1) } }
+
+// 4-4 sequence 함수 구현
+fun <A> sequence(a: List<Option<A>>): Option<List<A>> =
+    traverse(a) { it }
+
+fun <A, B> traverse(xa: List<A>, f: (A) -> Option<B>): Option<List<B>> =
+    when (xa) {
+        is Nil -> Some(Nil)
+        is Cons -> map2( f(xa.head), traverse(xa.tail, f)) { b, bs -> Cons(b, bs) }
+    }
+
+// 4-5 sequence 함수를 traverse를 이용해서 구현
+fun <A> sequence2(a: List<Option<A>>): Option<List<A>> =
+    traverse(a) { it }
+
+sealed class Either<out E, out A> {
+    companion object {
+        fun <E, A> of(a: A): Either<E, A> = Right(a)
+    }
+}
+
+data class Left<out E>(val value: E) : Either<E, Nothing>()
+data class Right<out A>(val value: A) : Either<Nothing, A>()
+
+// 4-6 Either 에 대한 모든 함수 구현  <-- f 가 항상 성공 ??
+fun <E, A, B> Either<E, A>.map(f: (A) -> B): Either<E, B> =
+    when (this) {
+        is Left -> this
+        is Right -> Right(f(this.value))
+    }
+
+fun <E, A, B> Either<E, A>.flatMap(f: (A) -> Either<E, B>): Either<E, B> =
+    when (this) {
+        is Left -> this
+        is Right -> f(this.value)
+    }
+
+fun <E, A, B, C> map2(a: Either<E, A>, b: Either<E, B>, f: (A, B) -> C): Either<E, C> =
+    a.flatMap { a1 -> b.map { b1 -> f(a1, b1) } }
+
+fun <E, A> Either<E, A>.orElse(b: () -> Either<E, A>): Either<E, A> =
+    when (this) {
+        is Left -> b()
+        is Right -> this
+    }
+
+// 4-7 Either 에 대한 traverse 함수 구현, 오류가 생긴 경우 최초로 발생한 오류 반환
+fun <E, A, B> traverseEither(xa: List<A>, f: (A) -> Either<E, B>): Either<E, List<B>> =
+    when (xa) {
+        is Nil -> Right(Nil)
+        is Cons -> map2( f(xa.head), traverseEither(xa.tail, f)) { b, bs -> Cons(b, bs) }
+    }
+
+// 4-7 Either 에 대한 sequence 함수 구현
+fun <E, A> sequence(a: List<Either<E, A>>): Either<E, List<A>> =
+    traverseEither(a) { it }
+
+
+// ================================================================================
+// 3장 리스트를 4장에서 사용하기 위해 복사
+
+
 sealed class List<out A> {
     companion object {
         fun <A> of(vararg aa: A): List<A> {
@@ -12,6 +132,16 @@ data class Cons<out A>(val head: A, val tail: List<A>) : List<A>()
 fun sum(list: List<Int>): Int = when (list) {
     is Nil -> 0
     is Cons -> list.head + sum(list.tail)
+}
+
+fun sum(list: List<Double>): Double = when (list) {
+    is Nil -> 0.0
+    is Cons -> list.head + sum(list.tail)
+}
+
+fun <T> size(list: List<T>): Int = when (list) {
+    is Nil -> 0
+    is Cons -> 1 + size(list.tail)
 }
 
 fun product(list: List<Double>): Double = when (list) {
@@ -68,15 +198,33 @@ fun <A, B> foldRight(xs: List<A>, acc: B, f: (A, B) -> B): B = when (xs) {
     is Cons -> f(xs.head, foldRight(xs.tail, acc, f))//.also { println(it) }  // 꼬리재귀 인가 vs 아닌가???? -> 아닌듯. 마지막이 f() 이기 때문에.
 }
 
-
+fun <A, B> foldRight2(xs: List<A>, acc: B, f: (A, B) -> B, check: (A) -> Boolean): B = when (xs) {
+    is Nil -> acc
+    is Cons ->
+        when (check(xs.head)) {
+            true -> acc
+            false -> f(xs.head, foldRight2(xs.tail, acc, f, check)).also { println(it) }
+        }
+}
 
 // 3-6 foldRight 를 이용한 product 구현 - 숏서킷 가능한가??  -> 그냥은 안됨. 숏서킷 조건을 foldRight 에 param 으로 던져주면 될듯. -> 안됨. foldRight 에서는 숏서킷을 하지 않는다.
 fun productByFoldRight(list: List<Double>): Double =
     foldRight(list, 1.0) {
-        a, acc -> if (a == 0.0) 0.0 else (a * acc)
+            a, acc -> if (a == 0.0) 0.0 else (a * acc)
     }
 
+fun productByFoldRight2(list: List<Double>): Double =
+    foldRight2(list, 1.0, {
+            a, acc -> if (a == 0.0) 0.0 else (a * acc)
+    }) { it == 0.0}
+
 fun <A> empty(): List<A> = Nil
+
+fun <A> isEmpty(xs: List<A>): Boolean = when (xs) {
+    is Nil -> true
+    is Cons -> false
+}
+
 // 3-7
 
 // 3-8 foldRight 를 이용한 length 구현
@@ -128,7 +276,7 @@ fun <A, B> flatMap(xs: List<A>, f: (A) -> List<B>): List<B> = flatten(map(xs, f)
 fun <A> filterByFlatMap(xs: List<A>, f: (A) -> Boolean): List<A> = flatMap(xs) { if (f(it)) List.of(it) else empty() }
 
 // 3-21, 22 List 두개를 받아서 해당 리스트의 각 원소를 더한 리스트를 반환하는 함수
-fun <A> zipWith(xs1: List<A>, xs2: List<A>, f: (A, A) -> A): List<A> = when (xs1) {
+fun <A, B, R> zipWith(xs1: List<A>, xs2: List<B>, f: (A, B) -> R): List<R> = when (xs1) {
     is Nil -> Nil
     is Cons -> when (xs2) {
         is Nil -> Nil
@@ -156,7 +304,7 @@ fun <A> hasSubsequence(xs: List<A>, sub: List<A>): Boolean = when (xs) {
         else {
             hasSubsequence(xs.tail, sub)
         }
-    }
+}
 
 sealed class Tree<out A>
 data class Leaf<out A>(val value: A) : Tree<A>()
@@ -169,7 +317,7 @@ fun <A> size(tree: Tree<A>): Int = when (tree) {
 }
 
 // 3-25 maximum 함수 구현
-fun maximum(tree: Tree<Int>): Int = when (tree) {
+fun <T:Comparable<T>> maximum(tree: Tree<T>): T = when (tree) {
     is Leaf -> tree.value
     is Branch -> maxOf(maximum(tree.left), maximum(tree.right))
 }
@@ -195,7 +343,7 @@ fun <A, B> fold(tree: Tree<A>, f: (A) -> B, g: (B, B) -> B): B = when (tree) {
 fun sizeF(tree: Tree<Int>): Int = fold(tree, { 1 }, { a, b -> a + b + 1 })
 
 // maximumF
-fun maximumF(tree: Tree<Int>): Int = fold(tree, { it }, { a, b -> maxOf(a, b) })
+fun <T: Comparable<T>> maximumF(tree: Tree<T>): T = fold(tree, { it }, { a, b -> maxOf(a, b) })
 
 // depthF
 fun depthF(tree: Tree<Int>): Int = fold(tree, { 1 }, { a, b -> maxOf(a, b) + 1 })
@@ -204,192 +352,8 @@ fun depthF(tree: Tree<Int>): Int = fold(tree, { 1 }, { a, b -> maxOf(a, b) + 1 }
 fun <A, B> mapF(tree: Tree<A>, f: (A) -> B): Tree<B> =
     fold(tree, { Leaf(f(it)) }, { a: Tree<B>, b: Tree<B> -> Branch(a, b) })
 
+
 fun main() {
-    val list = List.of(1, 2, 3, 4, 5)
-    println("3-1")
-    println(tail(list))
-    println(tail(empty<Int>()))
-    println()
+    println("start main")
 
-    println("3-2")
-    println(setHead(list, 10))
-    println(setHead(empty(), 10))
-    println()
-
-    println("3-3")
-    println(drop(list, 3))
-    println(drop(empty<Int>(), 3))
-    println(drop(list, 0))
-    println(drop(list, 7))
-    println()
-
-    println("3-4")
-    println(dropWhile(list) { it < 3 })
-    println(dropWhile(list) {it < 6})
-    println(dropWhile(empty<Int>()){it < 6})
-    println(dropWhile(list) {it > 6})
-    println()
-
-    println("3-5")
-    println(init(list))
-    println(init(Nil))
-    println(init(List.of(1)))
-    println()
-
-    val doubleList = List.of(1.0, 2.0, 3.0, 4.0, 5.0)
-    println("3-6")
-    println(productByFoldRight(doubleList))
-    println(productByFoldRight(List.of(1.0, 2.0, 3.0, 0.0, 5.0)))
-    println(productByFoldRight(empty<Double>()))
-    println()
-
-    println("3-7")
-    println(foldRight(
-        List.of(1, 2, 3),
-        empty<Int>()
-    ) { a, b -> Cons(a, b) })
-    println()
-
-    println("3-8")
-    println(length(list))
-    println(length(empty<Int>()))
-    println()
-
-    println("3-9")
-    println(foldLeft(list, 0) { b, a -> b + a })
-    println(foldLeft(empty<Int>(), 0) { b, a -> b + a })
-    println()
-
-    println("3-10")
-    println(sumByFoldLeft(list))
-    println(sumByFoldLeft(empty()))
-    println(productByFoldLeft(doubleList))
-    println(productByFoldLeft(empty()))
-    println(lengthByFoldLeft(list))
-    println(lengthByFoldLeft(empty<Int>()))
-    println()
-
-    println("3-11")
-    println(reverse(list))
-    println(reverse(empty<Int>()))
-    println(reverse2(list))
-    println(reverse2(empty<Int>()))
-    println()
-
-    println("3-12")
-    println(foldRightByFoldLeft(list, empty<Int>()) { a, b -> Cons(a, b) })
-    println(foldRightByFoldLeft(empty<Int>(), empty<Int>()) { a, b -> Cons(a, b) })
-    println(foldLeftByFoldRight(list, empty<Int>()) { b, a -> Cons(a, b) })
-    println(foldLeftByFoldRight(empty<Int>(), empty<Int>()) { b, a -> Cons(a, b) })
-    println()
-
-    println("3-13")
-    println(appendByFoldLeft(list, List.of(6, 7, 8)))
-    println(appendByFoldLeft(empty<Int>(), List.of(6, 7, 8)))
-    println(appendByFoldRight(list, List.of(6, 7, 8)))
-    println(appendByFoldRight(empty<Int>(), List.of(6, 7, 8)))
-    println()
-
-    println("3-14")
-    println(flatten(List.of(List.of(1, 2, 3), List.of(4, 5, 6), List.of(7, 8, 9))))
-    println(flatten(List.of(List.of(1, 2, 3), empty(), List.of(7, 8, 9), List.of(10, 11, 12))))
-    println()
-
-    println("3-15")
-    println(mapAddOne(list))
-    println(mapAddOne(empty()))
-    println()
-
-    println("3-16")
-    println(mapDouble2String(doubleList))
-    println(mapDouble2String(empty()))
-    println()
-
-    println("3-17")
-    println(map(list) { it + 1 })
-    println(map(empty<Int>()) { it + 1 })
-    println(map(doubleList) { it.toString() })
-    println(map(empty<Double>()) { it.toString() })
-    println()
-
-    println("3-18")
-    println(filter(list) { it % 2 == 0 })
-    println(filter(empty<Int>()) { it % 2 == 1 })
-    println()
-
-    println("3-19")
-    println(flatMap(list) { List.of(it, it) })
-    println(flatMap(empty<Int>()) { List.of(it, it) })
-    println()
-
-    println("3-20")
-    println(filterByFlatMap(list) { it % 2 == 0 })
-    println(filterByFlatMap(empty<Int>()) { it % 2 == 1 })
-    println()
-
-    println("3-21")
-    println(zipWith(list, List.of(6, 7, 8)) { a, b -> a + b })
-    println(zipWith(list, empty()) { a, b -> a + b })
-    println()
-
-    println("3-23")
-    println(hasSubsequence(list, List.of(2, 3)))
-    println(hasSubsequence(list, List.of(2, 4)))
-    println(hasSubsequence(list, empty()))
-    println(hasSubsequence(empty<Int>(), empty()))
-    println(hasSubsequence(empty(), List.of(2, 4)))
-    println()
-
-
-    println("3-24")
-    val tree = Branch(
-        Branch(
-            Leaf(1),
-            Leaf(2)
-        ),
-        Branch(
-            Leaf(3),
-            Leaf(4)
-        )
-    )
-    println(size(tree))
-    println(size(Leaf(1)))
-    println(size(Branch(Leaf(1), Leaf(2))))
-    println(size(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))))
-    println(size(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))))
-    println()
-
-
-    println("3-25")
-    val tree2 = Branch(
-        Branch(
-            Leaf(1),
-            Leaf(2)
-        ),
-        Branch(
-            Branch(Leaf(4),
-                Branch(Leaf(5),
-                    Leaf(6)),
-            ),
-            Branch(
-                Leaf(4),
-                Leaf(5)
-            )
-        )
-    )
-    println(maximum(tree2))
-    println()
-
-    println("3-26")
-    println(depth(tree2))
-    println()
-
-    println("3-27")
-    println(map(tree2) { it + 1 })
-    println()
-
-    println("3-28")
-    println(fold(tree2, { it+1 }, { a, b -> a + b }))
-    println(fold(tree2, { it }, { a, b -> a + b }))
-    println()
 }
