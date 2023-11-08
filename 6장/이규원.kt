2037b1d5ff694d7924e6c4eddb023f5daa5b45e7
leekyuwon <kyuwoon369@gmail.com>
2023-11-08 18:40:49 +0900

    6장 연습문제 부록 타이핑

---
+++ 6장/이규원.kt
@@ -0,0 +1,19 @@
+package com.kotlin.study.kotlinfp
+
+interface RNG {
+    fun nextInt(): Pair<Int, RNG>
+}
+
+data class SimpleRNG(val seed: Long) : RNG {
+    override fun nextInt(): Pair<Int, RNG> {
+        val newSeed = (seed * 0x5DEECE66DL + 0xBL) and 0xFFFFFFFFFFFFL
+        val nextRNG = SimpleRNG(newSeed)
+        val n = (newSeed ushr 16).toInt()
+        return n to nextRNG
+    }
+}
+
+fun nonNegativeInt(rng: RNG): Pair<Int, RNG> {
+    val (number, rng2) = rng.nextInt()
+    return (if (number < 0) -(number + 1) else number) to rng2
+}
