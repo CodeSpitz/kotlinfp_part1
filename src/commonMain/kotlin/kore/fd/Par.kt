bf93b22600318b668213632d0f5f68f025164359
hika <hika00@gmail.com>
2024-01-23 13:21:21 +0900

    .

--- src/commonMain/kotlin/kore/fd/Par.kt
+++ src/commonMain/kotlin/kore/fd/Par.kt
@@ -1,35 +1,74 @@
-@file:Suppress("UNCHECKED_CAST")
-
 package kore.fd
 
-import kore.fd.Par.Companion.fork
+import java.util.concurrent.Callable
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-class Par<out V:Any>private constructor(private val value:Any){
-    companion object{
-        operator fun <VALUE:Any>invoke(block:Thunk<VALUE>):Par<VALUE> = Par(block)
-        operator fun <VALUE:Any> invoke(value:VALUE):Par<VALUE> = Par(value)
-        fun <V:Any> fork(block:()->Par<V>):Par<V> = Par{block().get}
+class Actor<T>(private val block:(T)->Unit){
+    private var queue:ConcurrentLinkedQueue<T> = ConcurrentLinkedQueue()
+    private var running:Boolean = false
+    private var executor:ExecutorService = Executors.newFixedThreadPool(1)
+    private fun run(){
+        if(running) return
+        running = true
+        executor.submit(Callable{
+            while(true){
+                val head = queue.poll()
+                if(head == null){
+                    running = false
+                    return@Callable
+                }
+                block(head)
+            }
+        })
     }
-    val get:V get() = when(value){
-        is Thunk<*>->value.invoke() as V
-        else->value as V
+    operator fun invoke(t:T){
+        queue.add(t)
+        run()
     }
-    fun run():V = get
 }
-
+interface Runner{
+    operator fun <VALUE:Any> invoke(block:()->VALUE):Defer<VALUE>
+}
+fun interface Defer<out VALUE:Any>:()->VALUE
+fun interface Par<VALUE:Any>:(Runner)->Defer<VALUE>{
+    companion object{
+        operator fun <VALUE:Any>invoke(block:(Runner)->Defer<VALUE>):Par<VALUE> = Par(block)
+        operator fun <VALUE:Any> invoke(value:VALUE):Par<VALUE> = Par{Defer{value}}
+        fun <V:Any> fork(block:()->Par<V>):Par<V> = Par{block()(it)}
+    }
+    fun <OTHER:Any, RESULT:Any> map2(target:Par<OTHER>, block:(VALUE, OTHER)->RESULT):Par<RESULT>
+    = Par{
+        var i = 0
+        var a:Any = 0
+        var b:Any = 0
+        val actor = Actor<Par<*>>{par->
+            if(i == 0){
+                a = par(it)()
+            }
+            else if(i == 1){
+                b = par(it)()
+            }
+            i++
+        }
+        actor(this)
+        actor(target)
+        Defer {
+            while(i < 2) Thread.sleep(1)
+            block(a as VALUE, b as OTHER)
+        }
+    }
+}
 
 fun sum(ints:List<Int>):Par<Int>
-= if(ints.isEmpty()) Par(0)
+= if(ints.size == 1) Par(ints[0])
 else{
     val (l, r) = ints.splitAt(ints.size / 2)
-    //Par(sum(l).get + sum(r).get)
-    fork{sum(l)}.map2(fork{sum(r)}){a, b->a + b}
-}
-fun <ORIGIN:Any, TARGET:Any, RESULT:Any> Par<ORIGIN>.map2(target:Par<TARGET>, block:(ORIGIN, TARGET)->RESULT):Par<RESULT>{
-    return Par(block(get, target.get))
+    Par.fork{sum(l)}.map2(Par.fork{sum(r)}){a, b->a + b}
 }
 
-private fun <V:Any> List<V>.splitAt(i:Int):List<List<V>> {
+fun <V:Any> List<V>.splitAt(i:Int):List<List<V>> {
     val l:List<V> = take(i)
     val r:List<V> = drop(i)
     return listOf(l, r)
--- src/jvmTest/kotlin/fd/ParTest.kt
+++ src/jvmTest/kotlin/fd/ParTest.kt
@@ -1,13 +1,32 @@
 package fd
 
 import kore.fd.*
+import java.util.concurrent.Callable
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+class Executor(private val executor:ExecutorService):Runner{
+    override fun <VALUE:Any> invoke(block:()->VALUE):Defer<VALUE> = Defer{executor.submit(Callable{block()}).get()}
+}
 class ParTest {
     @Test
     fun test1(){
-        val par = Par{1}
-        assertEquals(1, par.get)
+        val par = Par(1)
+        val ex = Executor(Executors.newFixedThreadPool(1))
+        assertEquals(1, par(ex)())
+    }
+    @Test
+    fun test2(){
+        val a = sum(listOf(1,2,3,4,5,6,7,8,9,10))(Executor(Executors.newFixedThreadPool(1)))()
+        assertEquals(55, a)
     }
 }
+fun <V> List<V>.splitAt(i:Int):List<List<V>> {
+    val l:List<V> = take(i)
+    val r:List<V> = drop(i)
+    return listOf(l, r)
+}
+
